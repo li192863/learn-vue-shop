060b3be176ed1450953fc0bea7014e64c870d246
Chestnut <li1928631@163.com>
2022-09-22 21:46:39 +0800

    add progress bar.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+import NProgress from 'nprogress' // 导入进度条
+import 'nprogress/nprogress.css'
+
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
@@ -10,21 +13,16 @@ axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 请求拦截器
 axios.interceptors.request.use(config => {
   // Do something before request is sent
+  NProgress.start() // 显示进度条
   config.headers.Authorization = window.sessionStorage.getItem('token') // 为请求头添加字段（token令牌）
   return config // 最后必须返回config
-}
-)
+})
 
 // Add a response interceptor
-axios.interceptors.response.use(
-  function (response) {
-    // Do something with response data
-    return response
-  },
-  function (error) {
-    // Do something with response error
-    return Promise.reject(error)
-  }
-)
+axios.interceptors.response.use(config => {
+  // Do something with response data
+  NProgress.done() // 关闭进度条
+  return config
+})
 
 Vue.prototype.$http = axios // 将axios挂载至Vue的原型对象，可作为属性使用
